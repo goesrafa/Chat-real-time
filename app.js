581ba27bb7271f9bb32c9f28844bb9899f20ac77
goesrafa <rafa.goes.2012@gmail.com>
2021-12-06 13:10:45 -0300

    Atualizações

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ var server = app.listen(80, function(){
     console.log('Server online');
 })
 
-require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/**Criação da conexão por websocket */
+io.on('connection', function(socket){
+
+    socket.on('disconnect', function(){
+    });
+
+});
